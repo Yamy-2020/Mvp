695dfe0cfeca6082affbe246eb05cb9b3dbfc519
Yamy-2020 <2577385715@qq.com>
2020-06-02 15:41:21 +0800

    MVP抽取

--- app/src/main/java/com/example/mvp/base/BaseObserver.java
+++ app/src/main/java/com/example/mvp/base/BaseObserver.java
@@ -15,15 +15,15 @@ public abstract class BaseObserver implements Observer {
     public void onSubscribe(Disposable d) {
         mDisposable = d;
     }
-
+    //加载数据
     @Override
     public void onNext(Object o) {
         onSuccess(o);
         disposable();
     }
-
+    //成功
     protected abstract void onSuccess(Object o);
-
+    //失败
     @Override
     public void onError(Throwable e) {
         onFail(e);
